5777f072fd7960a89cdc2dbe29471a9ea01f4c3c
Yuri Adams <yuriadams@gmail.com>
2015-10-19 16:10:50 -0300

    implantando o package httprouter. Ainda não está registrando as statistics

--- server.go
+++ server.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/yuriadams/shorter/url"
 )
 
@@ -17,36 +17,43 @@ var (
 	urlBase string
 )
 
+var stats chan string
+
 func init() {
 	domain := flag.String("d", "localhost", "domain")
 	port = flag.Int("p", 8888, "port")
 	logOn = flag.Bool("l", true, "log on/off")
 
 	flag.Parse()
-
 	urlBase = fmt.Sprintf("http://%s:%d", *domain, *port)
 }
 
 type Headers map[string]string
 
-type Dispatcher struct {
-	stats chan string
+func main() {
+	r := httprouter.New()
+	stats := make(chan string)
+	defer close(stats)
+	go saveStatistics(stats)
+
+	r.GET("/r/:id", DispatchHandler)
+	r.POST("/api/short", CreateShortedURLHandler)
+	r.GET("/api/stats/:id", ViewStatisticsHandler)
+
+	logging("Starting server %d...", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), r))
 }
 
-func (r *Dispatcher) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	findURLAndExecute(w, req, func(url *url.URL) {
-		http.Redirect(w, req, url.Destino, http.StatusMovedPermanently)
-		r.stats <- url.ID
+func DispatchHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	findURLAndExecute(w, r, p, func(url *url.URL) {
+		http.Redirect(w, r, url.Destino, http.StatusMovedPermanently)
+
+		// stats <- url.ID
 	})
 }
 
-func Shorter(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		respondWith(w, http.StatusMethodNotAllowed, Headers{"Allow": "POST"})
-		return
-	}
-
-	url, nova, err := url.FindORCreateURL(extractURL(r))
+func CreateShortedURLHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	url, new, err := url.FindORCreateURL(extractURL(r))
 
 	if err != nil {
 		respondWith(w, http.StatusBadRequest, nil)
@@ -54,24 +61,31 @@ func Shorter(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var status int
-	if nova {
+	if new {
 		status = http.StatusCreated
 	} else {
 		status = http.StatusOK
 	}
 
-	urlCurta := fmt.Sprintf("%s/r/%s", urlBase, url.ID)
+	urlShorted := fmt.Sprintf("%s/r/%s", urlBase, url.ID)
 
 	respondWith(w, status, Headers{
-		"Location": urlCurta,
+		"Location": urlShorted,
 		"Link":     fmt.Sprintf("<%s/api/stats/%s>; rel=\"stats\"", urlBase, url.ID),
 	})
 
-	logging("URL %s shorted with sucess to %s.", url.Destino, urlCurta)
+	logging("URL %s shorted with sucess to %s.", url.Destino, urlShorted)
 }
 
-func Viewer(w http.ResponseWriter, r *http.Request) {
-	findURLAndExecute(w, r, func(url *url.URL) {
+func saveStatistics(stats <-chan string) {
+	for id := range stats {
+		url.SaveClick(id)
+		logging("Click saved with success %s.", id)
+	}
+}
+
+func ViewStatisticsHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	findURLAndExecute(w, r, p, func(url *url.URL) {
 		json, err := json.Marshal(url.Stats())
 
 		if err != nil {
@@ -83,15 +97,19 @@ func Viewer(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func findURLAndExecute(w http.ResponseWriter, r *http.Request, executor func(*url.URL)) {
-	path := strings.Split(r.URL.Path, "/")
-	id := path[len(path)-1]
-
+func findURLAndExecute(w http.ResponseWriter, r *http.Request, p httprouter.Params, executor func(*url.URL)) {
+	id := p.ByName("id")
 	if url := url.FindByID(id); url != nil {
 		executor(url)
 	} else {
 		http.NotFound(w, r)
 	}
+}
+
+func extractURL(r *http.Request) string {
+	rawBody := make([]byte, r.ContentLength, r.ContentLength)
+	r.Body.Read(rawBody)
+	return string(rawBody)
 }
 
 func respondWith(w http.ResponseWriter, status int, headers Headers) {
@@ -106,34 +124,8 @@ func respondWithJSON(w http.ResponseWriter, resposta string) {
 	fmt.Fprintf(w, resposta)
 }
 
-func extractURL(r *http.Request) string {
-	rawBody := make([]byte, r.ContentLength, r.ContentLength)
-	r.Body.Read(rawBody)
-	return string(rawBody)
-}
-
-func saveStatistics(stats <-chan string) {
-	for id := range stats {
-		url.SaveClick(id)
-		logging("Click saved with success %s.", id)
-	}
-}
-
-func logging(formato string, valores ...interface{}) {
+func logging(format string, v ...interface{}) {
 	if *logOn {
-		log.Printf(fmt.Sprintf("%s\n", formato), valores...)
+		log.Printf(fmt.Sprintf("%s\n", format), v...)
 	}
-}
-
-func main() {
-	stats := make(chan string)
-	defer close(stats)
-	go saveStatistics(stats)
-
-	http.Handle("/r/", &Dispatcher{stats})
-	http.HandleFunc("/api/short", Shorter)
-	http.HandleFunc("/api/stats/", Viewer)
-
-	logging("Starting server %d...", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
